14d5a8eb5fba146a8989cbc3681e89494a478a50
primezhang <primezhang@tencent.com>
2021-11-24 12:05:16 +0800

    init

---
+++ orderedmap.go
@@ -0,0 +1,124 @@
+package main
+
+type ListNode struct {
+	before *ListNode
+	after  *ListNode
+	key    interface{}
+	val    interface{}
+}
+
+type OrderedMap struct {
+	dataMap     map[interface{}]interface{}
+	head        *ListNode
+	accessOrder bool // false按插入顺序排序，true按访问顺序
+}
+
+func NewOrderedMap() *OrderedMap {
+	lhm := &OrderedMap{make(map[interface{}]interface{}), nil, false}
+
+	return lhm
+}
+
+func (t *OrderedMap) Set(k, v interface{}) bool {
+	// 往尾部插入
+	t.addBefore(k, v)
+	t.dataMap[k] = v
+	return true
+}
+
+func (t *OrderedMap) addBefore(k, v interface{}) {
+	// 往尾部插入
+	node := &ListNode{
+		key: k,
+		val: v,
+	}
+
+	// 查找一波
+	if _, ok := t.dataMap[k]; ok {
+		if t.accessOrder == false {
+			return
+		}
+
+		// 在链表里删除
+		t.DelListNode(k)
+	}
+	// 插入头部
+	t.head, node = node, t.head
+	t.head.before = node
+
+	if node != nil {
+		node.after = t.head
+	}
+}
+
+func (t *OrderedMap) DelListNode(k interface{}) {
+	node := t.head
+	for node != nil {
+		if node.key != k {
+			node = node.before
+			continue
+		}
+
+		if node.before != nil {
+			node.before.after = node.after
+		}
+		if node.after != nil {
+			node.after.before = node.before
+		}
+		if node.before == nil && node.after == nil {
+			t.head = nil
+		}
+		break
+	}
+	return
+}
+
+func (t *OrderedMap) Get(k interface{}) (interface{}, bool) {
+	v, ok := t.dataMap[k]
+	if t.accessOrder && ok {
+		t.addBefore(k, v)
+	}
+
+	return v, ok
+}
+
+func (t OrderedMap) Keys() []interface{} {
+	keys := []interface{}{}
+	if t.head == nil {
+		return keys
+	}
+
+	node := t.head
+	for node != nil {
+		if t.accessOrder {
+			keys = append(keys, node.key)
+		} else {
+			keys = append([]interface{}{node.key}, keys...)
+		}
+		node = node.before
+	}
+	return keys
+}
+
+func (t OrderedMap) Del(k interface{}) {
+	t.DelListNode(k)
+	delete(t.dataMap, k)
+}
+
+// -------------- 快速计数 ----------
+func (t *OrderedMap) Add(k interface{}, num int) bool {
+	v, ok := t.Get(k)
+	if !ok {
+		return false
+	}
+	oriNum, ok := v.(int)
+	if !ok {
+		return false
+	}
+
+	return t.Set(k, oriNum+num)
+}
+
+func (t *OrderedMap) Sub(k interface{}, num int) bool {
+	return t.Add(k, -num)
+}
---
+++ orderedmap_test.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	//"fmt"
+
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestNewOrderedMap(t *testing.T) {
+	m := NewOrderedMap()
+	assert.IsType(t, &OrderedMap{}, m)
+}
+
+func TestGet(t *testing.T) {
+	t.Run("ReturnsNotOKIfStringKeyDoesntExist", func(t *testing.T) {
+		m := NewOrderedMap()
+		_, ok := m.Get("foo")
+		assert.False(t, ok)
+	})
+
+	t.Run("ReturnsNotOKIfNonStringKeyDoesntExist", func(t *testing.T) {
+		m := NewOrderedMap()
+		_, ok := m.Get(123)
+		assert.False(t, ok)
+	})
+
+	t.Run("ReturnsOKIfKeyExists", func(t *testing.T) {
+		m := NewOrderedMap()
+		m.Set("foo", "bar")
+		_, ok := m.Get("foo")
+		assert.True(t, ok)
+	})
+
+	t.Run("ReturnsValueForKey", func(t *testing.T) {
+		m := NewOrderedMap()
+		m.Set("foo", "bar")
+		value, _ := m.Get("foo")
+		assert.Equal(t, "bar", value)
+	})
+
+	t.Run("ReturnsDynamicValueForKey", func(t *testing.T) {
+		m := NewOrderedMap()
+		m.Set("foo", "baz")
+		value, _ := m.Get("foo")
+		assert.Equal(t, "baz", value)
+	})
+
+	t.Run("KeyDoesntExistOnNonEmptyMap", func(t *testing.T) {
+		m := NewOrderedMap()
+		m.Set("foo", "baz")
+		_, ok := m.Get("bar")
+		assert.False(t, ok)
+	})
+
+	t.Run("ValueForKeyDoesntExistOnNonEmptyMap", func(t *testing.T) {
+		m := NewOrderedMap()
+		m.Set("foo", "baz")
+		value, _ := m.Get("bar")
+		assert.Nil(t, value)
+	})
+
+}
+
+func TestSet(t *testing.T) {
+	t.Run("ReturnsTrueIfStringKeyIsNew", func(t *testing.T) {
+		m := NewOrderedMap()
+		ok := m.Set("foo", "bar")
+		assert.True(t, ok)
+	})
+
+	t.Run("ReturnsTrueIfNonStringKeyIsNew", func(t *testing.T) {
+		m := NewOrderedMap()
+		ok := m.Set(123, "bar")
+		assert.True(t, ok)
+	})
+
+	t.Run("ValueCanBeNonString", func(t *testing.T) {
+		m := NewOrderedMap()
+		ok := m.Set(123, true)
+		assert.True(t, ok)
+	})
+
+	t.Run("SetThreeDifferentKeys", func(t *testing.T) {
+		m := NewOrderedMap()
+		m.Set("foo", "bar")
+		m.Set("baz", "qux")
+		ok := m.Set("quux", "corge")
+		assert.True(t, ok)
+	})
+
+}
+
+func TestAdd(t *testing.T) {
+	t.Run("Returns3IfAdd2", func(t *testing.T) {
+		m := NewOrderedMap()
+		m.Set("foo", 1)
+		m.Add("foo", 2)
+		got, _ := m.Get("foo")
+		assert.Equal(t, got, 3)
+	})
+
+	t.Run("ReturnsFalseIfValueIsNotInt", func(t *testing.T) {
+		m := NewOrderedMap()
+		m.Set("foo", "bar")
+		ok := m.Add("foo", 1)
+		assert.False(t, ok)
+	})
+}
